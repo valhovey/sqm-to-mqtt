b922a6f9b3c7d5f2a5e9b2e7b4ce0885bcbd5dbf
valhovey <khoveyc@gmail.com>
2026-03-05 11:22:10 -0700

    Created initial working example

---
+++ server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import serial
+import json
+
+def get_config():
+    with open('data.json', 'r') as file:
+        return json.load(file)
+
+config = get_config()
+PORT = config["serial-path"]
+BAUD = config["baudrate"]
+
+def get_reading():
+    with serial.Serial(
+            port=PORT,
+            baudrate=BAUD,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=2
+        ) as ser:
+        ser.reset_input_buffer()
+        ser.reset_output_buffer()
+        ser.write(b"rx\n")
+        line = ser.readline()
+
+        return line.decode(errors="replace").strip()
+
+if __name__ == "__main__":
+    reading = get_reading()
+
+    print(reading)
