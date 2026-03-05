6ea2ca6fac1fde4b27d4fa5b6042d7416b567e6a
valhovey <khoveyc@gmail.com>
2026-03-05 12:16:43 -0700

    Added moon stats

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+from skyfield.api import load, Topos
+from skyfield.almanac import fraction_illuminated
+from datetime import datetime, timezone
 import serial
 import json
 
@@ -10,6 +13,8 @@ def get_config():
 config = get_config()
 PORT = config["serial-path"]
 BAUD = config["baudrate"]
+LAT = config["lat"]
+LON = config["lon"]
 
 def get_reading():
     with serial.Serial(
@@ -27,6 +32,23 @@ def get_reading():
 
         return line.decode(errors="replace").strip()
 
+def get_moon_stats(now):
+    eph = load('de421.bsp')
+    moon = eph['moon']
+    earth = eph['earth']
+    observer = earth + Topos(latitude_degrees=LAT, longitude_degrees=LON)
+    astrometric = observer.at(now).observe(moon)
+    alt, az, _ = astrometric.apparent().altaz()
+    illum = fraction_illuminated(eph, 'moon', now)
+
+    moon = {
+        "alt_deg": round(alt.degrees, 2),
+        "az_deg": round(az.degrees, 2),
+        "illum": round(illum * 100, 2),
+    }
+
+    return moon
+
 def parse_reading(reading):
     values = list(map(str.strip, reading.split(",")))
     sqm = float(values[1][:-1])
@@ -43,7 +65,10 @@ def get_mock_reading():
     return "r, 09.66m,0000012099Hz,0000000000c,0000000.000s, 024.8C"
 
 if __name__ == "__main__":
+    ts = load.timescale()
+    now = ts.from_datetime(datetime.now(timezone.utc))
     reading = get_mock_reading()
     parsed = parse_reading(reading)
+    moon = get_moon_stats(now)
 
-    print(parsed)
+    print({**moon, **parsed})
