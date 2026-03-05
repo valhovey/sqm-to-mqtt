f6d211ce1a48ed1627c0330eed3e6935fec572b0
valhovey <khoveyc@gmail.com>
2026-03-05 11:58:16 -0700

    Added data parsing

--- server.py
+++ server.py
@@ -27,7 +27,23 @@ def get_reading():
 
         return line.decode(errors="replace").strip()
 
-if __name__ == "__main__":
-    reading = get_reading()
+def parse_reading(reading):
+    values = list(map(str.strip, reading.split(",")))
+    sqm = float(values[1][:-1])
+    temp = float(values[5][:-1])
 
-    print(reading)
+    parsed = {
+        "sqm": sqm,
+        "temp_c": temp
+    }
+
+    return parsed
+
+def get_mock_reading():
+    return "r, 09.66m,0000012099Hz,0000000000c,0000000.000s, 024.8C"
+
+if __name__ == "__main__":
+    reading = get_mock_reading()
+    parsed = parse_reading(reading)
+
+    print(parsed)
